1797ab2608ce9ded135b034c7c4db0033a61fb11
Illia Onishchenko <pearljam546@gmail.com>
2020-04-23 21:11:01 +0300

    Updates

--- js/speakersModals.js
+++ js/speakersModals.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (key == 27) {
 
             document.querySelector('.modal.active').classList.remove('active');
-            document.querySelector('.overlay').classList.remove('active');
+            overlay.classList.remove('active');
             
             document.body.style.overflow = 'auto';
         };
