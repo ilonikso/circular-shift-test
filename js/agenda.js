94151dfbd543978f3adab52a5cce876586ad86fe
Illia Onishchenko <pearljam546@gmail.com>
2020-04-23 11:15:32 +0300

    Updates

---
+++ js/agenda.js
@@ -0,0 +1,15 @@
+let acc = document.getElementsByClassName("agenda__header-block");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
+  });
+}
